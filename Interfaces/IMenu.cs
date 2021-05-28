a60474f69004bf8f07cf47e92ee670aa5c63da62
EbicHecker <60292167+epic6969@users.noreply.github.com>
2021-05-29 01:27:00 +0200

    Delete IMenu.cs
    Not used

--- Interfaces/IMenu.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LogicDeobfuscator.Interfaces
-{
-    public interface IMenu
-    {
-        public string Name { get; }
-        public void Show();
-        public void Hide();
-    }
-}
